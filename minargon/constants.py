3fa9aab5f47bf3d6418e7b78240e5c90b3ac9f40
Dominic Barker <djbarker@sbnd-daq30.fnal.gov>
2018-07-24 12:15:50 -0500

    addit channel map file

--- minargon/constants.py
+++ minargon/constants.py
@@ -24,7 +24,7 @@ SLOT_OFFSET = 4
 N_WIRES_PER_PLANE = 240
 
 # channel mapping
-CHANNEL_MAP_FNAME = "/web/sites/s/sbn-online.fnal.gov/cgi-bin/minargon/minargon/channel_map.txt"
+CHANNEL_MAP_FNAME = "./minargon/channel_map.txt"
 (channel_to_wire, wire_to_channel, N_CHANNELS_PER_FEM, FEM_ACTIVE_CHANNELS) = tools.parse_channel_map_file(CHANNEL_MAP_FNAME, N_CHANNELS_PER_FEM, N_FEM, SLOT_OFFSET)
 
 # (channel_to_wire, wire_to_channel, N_CHANNELS_PER_FEM, FEM_ACTIVE_CHANNELS) = tools.default_channel_map(N_CHANNELS, N_FEM)
