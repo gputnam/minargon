19c27c02a2b34fff1a9064faeeb9deb3d2f6e185
Gray Putnam <grayputnam@uchicago.edu>
2018-09-26 13:04:04 -0500

    cleanup old javascript

--- minargon/static/js/minargon/detector.js
+++
@@ -1,4 +0,0 @@
-function get_wire(crate_id, slot_id, channel_id, detector) {
-    var channel = slot_id * detector.n_channel_per_fem[slot_id] + channel_id;
-    return detector.channel_to_wire[channel];
-}
