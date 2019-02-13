c4de39fe797463741dd586e6436439568f35ffc3
gputnam <gputnam@sbndgpvm01.fnal.gov>
2019-02-13 14:39:11 -0600

    move test_pvs function into postgres api and fix typos

--- minargon/metrics/postgres_api.py
+++ minargon/metrics/postgres_api.py
@@ -15,7 +15,7 @@ from psycopg2.extras import RealDictCursor
 from minargon.tools import parseiso, parseiso_or_int, stream_args
 
 from minargon import app
-from flask import jsonify, request
+from flask import jsonify, request, render_template
 from datetime import datetime, timedelta # needed for testing only
 import time
 import calendar
@@ -31,9 +31,9 @@ for connection_name, config in postgres_instances.items():
     database_name = config["name"]
     host = config["host"]
     port = config["port"]
-    with open(app.config["EPICS_SECRET_KEY"],"r") as f:
-       u = (file.readline()).strip() # strip: removes leading and trailing chars
-       p = (file.readline()).strip()
+    with open(key) as f:
+        u = (f.readline()).strip() # strip: removes leading and trailing chars
+        p = (f.readline()).strip()
     # Connect to the database
     connection = psycopg2.connect(database=database_name, user=u, password=p, host=host, port=port)
     # store it
@@ -64,7 +64,7 @@ def postgres_query(ID, start_t, stop_t, connection, config):
 	t_interval = abs(stop_t - start_t) # Absolute value to fix stream args giving a negative value
 
 	# Database query to execute, times converted to unix [ms]
-	if (config["name"] == "SBNTESTSTAND"):
+	if (config["name"] == "sbnteststand"):
 		query="""SELECT extract(epoch from SMPL_TIME)*1000 AS SAMPLE_TIME,FLOAT_VAL AS VALUE
 			FROM SAMPLE WHERE CHANNEL_ID=%s AND (NOW()-SMPL_TIME)<('%s') ORDER BY SMPL_TIME;""" % ( ID, t_interval )
 	
@@ -150,3 +150,77 @@ def ps_series(connection, ID):
 	}
 
 	return jsonify(values=ret)
+
+# A test func for the PV Lists this translates the page made by bill to the Minargon webpage
+# and also updates the script to be more compatible with python
+@app.route('/<connection>/test_pv')
+@postgres_route
+def test_pv(connection):
+
+    database = connection[1]["name"]
+    connection = connection[0]
+
+    # Cursor allows python to execute a postgres command in the database session. 
+    cursor = connection.cursor() # Fancy way of using cursor
+
+    # Database command to execute
+    if (database == "sbndtestatnd"):
+        query="""
+        SELECT CHAN_GRP.NAME, SPLIT_PART(CHANNEL.NAME,'/',1), SPLIT_PART(CHANNEL.NAME,'/',2), CHANNEL_ID
+        FROM CHANNEL, CHAN_GRP
+        WHERE CHANNEL.GRP_ID = CHAN_GRP.GRP_ID 
+        ORDER BY CHAN_GRP.NAME, CHANNEL.NAME;
+        """
+    else:
+        query="""
+        SELECT DCS_PRD.CHAN_GRP.NAME,SPLIT_PART(DCS_PRD.CHANNEL.NAME,'/',1),
+        SPLIT_PART(DCS_PRD.CHANNEL.NAME,'/',2),DCS_PRD.CHANNEL.CHANNEL_ID
+        FROM DCS_PRD.CHANNEL,DCS_PRD.CHAN_GRP WHERE DCS_PRD.CHANNEL.GRP_ID=DCS_PRD.CHAN_GRP.GRP_ID 
+        ORDER BY DCS_PRD.CHAN_GRP.NAME,DCS_PRD.CHANNEL.NAME;
+        """
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    # some variables for bookkeeping
+    old = [" ", " ", " "]
+    tags = [0, 0, 0]
+    index = [ 0, 0, 0 ]
+
+    # Top level pydict
+    pydict =	{ 
+        "text" : ["Process_Variables"],
+        "expanded": "true",
+        "nodes" : []
+    }
+
+    # Create a python dictonary out of the database query
+    for row in rows:
+        # Header 1
+        if row[0] != old[0]: # only use chan name part 1 once in loop to avoid overcounting e.g. grab APC then skip block until CRYO
+            tags[0] = 0
+            tags[1] = 0
+            pydict["nodes"].append( {"expanded": "false", "text" : str(row[0]), "href": "#parent1","nodes" : [], "tags": [str(tags[0])]} ) # Top Level 
+            old[0] = row[0]         
+            index[0] = index[0] + 1 # Increment the index
+            index[1] = 0
+
+        # Header 2
+        if row[1] != old[1]: # only use chan name part 2 once in loop to avoid overcounting 
+            tags[1] = 0
+            pydict["nodes"][index[0] - 1 ]["nodes"].append( {"href":"#child","expanded": "false","tags":[str(tags[1])], "text" : str(row[1]), "nodes": []  } ) # Level 2
+            index[1] = index[1] + 1
+            tags[0] = tags[0] + 1
+            old[1] = row[1]
+
+        # Push back every time       
+        pydict["nodes"][index[0] - 1 ]["nodes"][index[1] - 1]["nodes"].append( {"text" : str(row[2]), "tags" : [str(tags[1])] , "href" : "/power_supply_single_stream/"+str(row[3]) }) # Level 3
+        index[2] = index[2] + 1
+        tags[1] = tags[1] + 1
+
+    data = json.dumps(pydict) # convert python dictonary to json format
+
+    return render_template('test_pvs.html', data=data)
+ 
+
+
