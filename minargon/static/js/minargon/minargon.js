d1d42deb53ef57dcb6433bb4f04fff2575028112
Gray Putnam <gputnam@icarus-gateway01.fnal.gov>
2020-09-03 10:47:15 -0500

    First draft of looking up flanges through TPC ID. Add in support for nested dropdowns.

---
+++ minargon/static/js/minargon/minargon.js
@@ -0,0 +1,21 @@
+// Basic utility code for the minargon website
+// Requires JQuery loaded
+
+  
+$(document).ready(function() {
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+  }
+  var $subMenu = $(this).next('.dropdown-menu');
+  $subMenu.toggleClass('show');
+
+
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-submenu .show').removeClass('show');
+  });
+
+
+  return false;
+});
+});
