ca351e242c159b99b58b242d7f6e9a09b0440972
gputnam <gputnam@sbndgpvm04.fnal.gov>
2020-11-20 15:40:11 -0600

    More updates from ICARUS deploy.

--- minargon/metrics/postgres_api.py
+++ minargon/metrics/postgres_api.py
@@ -555,7 +555,7 @@ def pv_internal(connection, link_name=None, ret_id=None):
 @postgres_route
 def get_icarus_cryo(connection):
   cursor = connection[0].cursor();
-  query = """select name,last_smpl_time,to_char(last_float_val,'99999D99') from dcs_prd.channel where grp_id=8"""
+  query = """select name,last_smpl_time,to_char(last_float_val,'99999D99') from dcs_prd.channel where grp_id=9"""
 
   cursor.execute(query);
   dbrows = cursor.fetchall();
